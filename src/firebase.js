c28717b1905771794d21e952052e3aa889e9f4bf
Sergey Lutsenko <lytsenko.89@gmail.com>
2024-04-10 16:41:23 +0300

    fix process.env

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.FIREBASE_DATABASE_URL,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-// };
 const firebaseConfig = {
-  apiKey: 'AIzaSyDdmlvgoQ_5ZkyRwdCW0Rd9mHoxB0e3GKk',
-  authDomain: 'learn-lingo-997d3.firebaseapp.com',
-  databaseURL:
-    'https://learn-lingo-997d3-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'learn-lingo-997d3',
-  storageBucket: 'learn-lingo-997d3.appspot.com',
-  messagingSenderId: '775572315743',
-  appId: '1:775572315743:web:226f6bdd74ea33c71345ea',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
