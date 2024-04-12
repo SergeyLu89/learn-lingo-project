77b2865475538d8c4bba74596d968193ce9e20fb
SergeyLu89 <130161910+SergeyLu89@users.noreply.github.com>
2024-04-12 14:22:56 +0000

    Deploying to gh-pages from @ SergeyLu89/learn-lingo-project@013af63b08b3be7988f0e5ee9717aca6b9522bbd 🚀

--- static/js/413.799f2291.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunklearn_lingo_project=self.webpackChunklearn_lingo_project||[]).push([[413],{413:function(e,n,r){r.r(n);var c=r(184);n.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)("h2",{children:"FAVORITE PAGE"})})}}}]);
-//# sourceMappingURL=413.799f2291.chunk.js.map
---
+++ static/js/413.b36712bc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklearn_lingo_project=self.webpackChunklearn_lingo_project||[]).push([[413],{4413:function(e,n,r){r.r(n);var c=r(184);n.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)("h2",{children:"FAVORITE PAGE"})})}}}]);
+//# sourceMappingURL=413.b36712bc.chunk.js.map
