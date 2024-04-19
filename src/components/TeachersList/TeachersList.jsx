c1b7c7b23e187908bd996627b56d8682a13da3f3
Sergey Lutsenko <lytsenko.89@gmail.com>
2024-04-19 09:05:30 +0300

    fix re render cards in TeachersList

--- src/components/TeachersList/TeachersList.jsx
+++ src/components/TeachersList/TeachersList.jsx
@@ -1,13 +1,12 @@
 import css from './TeachersList.module.css';
 import TeachersListItem from 'components/TeachersListItem/TeachersListItem';
-import { nanoid } from 'nanoid';
 
 const TeachersList = ({ teachers }) => {
   return (
     <>
       <ul className={css.teachersList}>
         {teachers.map(teacher => (
-          <TeachersListItem teacher={teacher} key={nanoid()} />
+          <TeachersListItem teacher={teacher} key={teacher.id} />
         ))}
       </ul>
     </>
