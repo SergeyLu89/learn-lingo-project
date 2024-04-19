78e324f72df346c814f3d1799c0b291a73aba338
Sergey Lutsenko <lytsenko.89@gmail.com>
2024-04-19 22:56:37 +0300

    fix errors in pagination

--- src/pages/TeachersPage/TeachersPage.jsx
+++ src/pages/TeachersPage/TeachersPage.jsx
@@ -1,5 +1,5 @@
 import css from './TeachersPage.module.css';
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 import {
   getDatabase,
   ref,
@@ -12,42 +12,49 @@ import {
 import TeachersList from 'components/TeachersList/TeachersList';
 
 const TeachersPage = () => {
-  const db = getDatabase();
   const [teachers, setTeachers] = useState([]);
   const [lastKey, setLastKey] = useState('');
 
-  const memoizedFetchData = useCallback(async dbRef => {
-    try {
-      const snapshot = await onValue(dbRef, snapshot => {
-        if (snapshot.exists()) {
-          const fetchedTeachers = [];
-          snapshot.forEach(childSnapshot => {
-            const childKey = childSnapshot.key;
-            const childData = childSnapshot.val();
-            fetchedTeachers.push({ id: childKey, ...childData });
-            setLastKey(childKey);
-          });
-          setTeachers(prevTeachers => [...prevTeachers, ...fetchedTeachers]);
-        } else {
-          console.log('No data available');
-        }
-      });
+  const db = getDatabase();
 
-      return () => snapshot.unsubscribe();
+  // const fetchInitialData = () => {
+  //   const dbRef = query(ref(db, 'teachers'), orderByKey(), endAt('4'));
+
+  //   fetchData(dbRef);
+  // };
+
+  const dbRef = query(ref(db, 'teachers'), orderByKey(), endAt('3'));
+
+  const fetchData = ref => {
+    try {
+      onValue(
+        ref,
+        snapshot => {
+          if (snapshot.exists()) {
+            const fetchedTeachers = [];
+            snapshot.forEach(childSnapshot => {
+              const childKey = childSnapshot.key;
+              const childData = childSnapshot.val();
+              fetchedTeachers.push({ id: childKey, ...childData });
+
+              setLastKey(childKey);
+            });
+            setTeachers(prevTeachers => [...prevTeachers, ...fetchedTeachers]);
+          } else {
+            console.log('No data available');
+          }
+        },
+        { onlyOnce: true }
+      );
     } catch (error) {
       console.error(error.message);
     }
-  }, []);
+  };
 
   useEffect(() => {
-    const fetchInitialData = async () => {
-      const dbRef = query(ref(db, 'teachers'), orderByKey(), endAt('3'));
-      await memoizedFetchData(dbRef);
-    };
-
-    fetchInitialData();
-    return () => onValue.unsubscribe();
-  }, [memoizedFetchData, db]);
+    fetchData(dbRef);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleLoadMore = () => {
     const newDbRef = query(
@@ -56,13 +63,17 @@ const TeachersPage = () => {
       startAfter(lastKey),
       endAt(String(Number(lastKey) + 4))
     );
-    memoizedFetchData(newDbRef);
+    fetchData(newDbRef);
   };
 
   return (
     <section className={css.teachersPageSection}>
       <TeachersList teachers={teachers} />
-      <button type="button" onClick={handleLoadMore}>
+      <button
+        type="button"
+        onClick={handleLoadMore}
+        className={css.LoadMoreBtn}
+      >
         Load more
       </button>
     </section>
