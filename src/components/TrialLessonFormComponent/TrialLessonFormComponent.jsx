00e94bc71f4dbdd374d5cb072330e5397dd66749
Sergey Lutsenko <lytsenko.89@gmail.com>
2024-04-23 17:14:40 +0300

    added styles on lesson form

--- src/components/TrialLessonFormComponent/TrialLessonFormComponent.jsx
+++ src/components/TrialLessonFormComponent/TrialLessonFormComponent.jsx
@@ -29,65 +29,100 @@ const TrialLessonFormComponent = ({ teacher }) => {
   };
 
   return (
-    <>
+    <div className={css.lessonFormWrapper}>
       <h2>Book trial lesson</h2>
-      <p>
+      <p className={css.lessonFormWrapperDescr}>
         Our experienced tutor will assess your current language level, discuss
         your learning goals, and tailor the lesson to your specific needs.
       </p>
-      <div>
-        <img src={avatar_url} alt={`${name} ${surname}`} />
-        <div>
+      <div className={css.teacherInfoBox}>
+        <img
+          src={avatar_url}
+          alt={`${name} ${surname}`}
+          className={css.teacherAvatar}
+        />
+        <div className={css.teacherNameBox}>
           {' '}
-          <p>Your teacher</p>
           <p>
+            <span>Your teacher</span>
+          </p>
+          <h4>
             {name} {surname}
-          </p>{' '}
+          </h4>{' '}
         </div>
-        <h3>What is your main reason for learning English?</h3>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={schema}
-          onSubmit={onFormSubmit}
-        >
-          <Form autoComplete="off" className={css.trialLessonForm}>
-            <div className={css.formRadioBox}>
-              <label>
-                <Field type="radio" name="reason" value="carier" checked />
-                Career and business
-              </label>
-              <label>
-                <Field type="radio" name="reason" value="kids" />
-                Lesson for kids
-              </label>
-              <label>
-                <Field type="radio" name="reason" value="abroad" />
-                Living abroad
-              </label>
-              <label>
-                <Field type="radio" name="reason" value="coursework" />
-                Exams and coursework
-              </label>
-              <label>
-                <Field type="radio" name="reason" value="hobby" />
-                Culture, travel or hobby
-              </label>
-            </div>
-
-            <Field type="text" name="name" placeholder="Full Name" />
-            <ErrorMessage name="name" component="span" />
-
-            <Field type="email" name="email" placeholder="Email" />
-            <ErrorMessage name="email" component="span" />
-
-            <Field type="tel" name="phone" placeholder="Phone number" />
-            <ErrorMessage name="phone" component="span" />
-
-            <button type="submit">Book</button>
-          </Form>
-        </Formik>
       </div>
-    </>
+      <h3>What is your main reason for learning English?</h3>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={schema}
+        onSubmit={onFormSubmit}
+      >
+        <Form autoComplete="off" className={css.trialLessonForm}>
+          <div className={css.formRadioBox}>
+            <label className={css.customRadio}>
+              <Field type="radio" name="reason" value="carier" checked />
+              <span>Career and business</span>
+            </label>
+            <label className={css.customRadio}>
+              <Field type="radio" name="reason" value="kids" />
+              <span>Lesson for kids</span>
+            </label>
+            <label className={css.customRadio}>
+              <Field type="radio" name="reason" value="abroad" />
+              <span>Living abroad</span>
+            </label>
+            <label className={css.customRadio}>
+              <Field type="radio" name="reason" value="coursework" />
+              <span>Exams and coursework</span>
+            </label>
+            <label className={css.customRadio}>
+              <Field type="radio" name="reason" value="hobby" />
+              <span>Culture, travel or hobby</span>
+            </label>
+          </div>
+          <div className={css.userInfoWrapper}>
+            <Field
+              type="text"
+              name="name"
+              placeholder="Full Name"
+              className={css.formInput}
+            />
+            <ErrorMessage
+              name="name"
+              component="span"
+              className={css.inputError}
+            />
+
+            <Field
+              type="email"
+              name="email"
+              placeholder="Email"
+              className={css.formInput}
+            />
+            <ErrorMessage
+              name="email"
+              component="span"
+              className={css.inputError}
+            />
+
+            <Field
+              type="tel"
+              name="phone"
+              placeholder="Phone number"
+              className={css.formInput}
+            />
+            <ErrorMessage
+              name="phone"
+              component="span"
+              className={css.inputError}
+            />
+          </div>
+          <button type="submit" className={css.formBtn}>
+            Book
+          </button>
+        </Form>
+      </Formik>
+    </div>
   );
 };
 
